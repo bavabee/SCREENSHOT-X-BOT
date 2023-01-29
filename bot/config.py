db4f15a6acdb735e1654cbd02dacdeb8121daa8e
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2023-01-29 13:22:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 class Config:
 
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    API_ID = int(os.environ.get('API_ID', '8564523'))
+    API_HASH = os.environ.get('API_HASH', '1a39289e7dc9aa69b32683dc2adfc875')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5397997424:AAEvZuVX_bR_lEuv-owl20WfNXoO14VqVlA')
     SESSION_NAME = os.environ.get('SESSION_NAME', ':memory:')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001425688221'))
+    DATABASE_URL = os.environ.get('DATABASE_URL', 'mongodb+srv://RF999:RF999@cluster0.uaznbh4.mongodb.net/?retryWrites=true&w=majority')
+    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '1989750989').split(' ')]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
     TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
